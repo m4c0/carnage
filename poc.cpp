c955150cff0cede989c2219a2a739f12e99c8169
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-08 17:11:44 +0000

    super small program for text-to-phonemes

--- poc.cpp
+++ poc.cpp
@@ -1,10 +1,31 @@
 #pragma leco tool
 
+import hai;
+import hashley;
 import jojo;
+import jute;
 import print;
 
-int main() {
-  auto dict_file = jojo::read_cstr("cmudict/cmudict.dict");
-  putln(dict_file.begin());
+struct unknown_word {};
 
+int main(int argc, char ** argv) {
+  hai::cstr file = jojo::read_cstr("cmudict/cmudict.dict");
+  hashley::niamh w2p { 1023 };
+
+  jute::view lines { file };
+  while (lines.size()) {
+    auto [line, rest] = lines.split('\n');
+    lines = rest;
+
+    auto [word, phonemes] = line.split(' ');
+    w2p[word] = phonemes.begin() - file.begin();
+  }
+
+  for (auto i = 1; i < argc; i++) {
+    auto w = jute::view::unsafe(argv[i]);
+    auto n = w2p[w];
+    if (!n) throw unknown_word {};
+    auto p = jute::view { file }.subview(n).after.split('\n').before;
+    putln(p);
+  }
 }
