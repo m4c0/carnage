24d31e878656e3d4a5f72d0fe44b558a04685e26
Eduardo Costa <m4c0@users.noreply.github.com>
2025-02-08 17:21:40 +0000

    moving code to a class

--- poc.cpp
+++ poc.cpp
@@ -8,24 +8,34 @@ import print;
 
 struct unknown_word {};
 
-int main(int argc, char ** argv) {
+class map {
   hai::cstr file = jojo::read_cstr("cmudict/cmudict.dict");
   hashley::niamh w2p { 101113 };
 
-  jute::view lines { file };
-  while (lines.size()) {
-    auto [line, rest] = lines.split('\n');
-    lines = rest;
+public:
+  map() {
+    jute::view lines { file };
+    while (lines.size()) {
+      auto [line, rest] = lines.split('\n');
+      lines = rest;
 
-    auto [word, phonemes] = line.split(' ');
-    w2p[word] = phonemes.begin() - file.begin();
+      auto [word, phonemes] = line.split(' ');
+      w2p[word] = phonemes.begin() - file.begin();
+    }
   }
+
+  [[nodiscard]] auto operator[](jute::view w) {
+    auto n = w2p[w];
+    if (!n) throw unknown_word {};
+    return jute::view { file }.subview(n).after.split('\n').before;
+  }
+};
+
+int main(int argc, char ** argv) {
+  map m {};
 
   for (auto i = 1; i < argc; i++) {
     auto w = jute::view::unsafe(argv[i]);
-    auto n = w2p[w];
-    if (!n) throw unknown_word {};
-    auto p = jute::view { file }.subview(n).after.split('\n').before;
-    putln(p);
+    putln(m[w]);
   }
 }
